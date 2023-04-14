494a458e852da2370374ed515d2ad332a5fdec31
hsluoyz <hsluoyz@users.noreply.github.com>
2023-04-14 13:17:31 +0000

    deploy: 2bd7ef5723cbf6da58a4905f389bd1a29359243f

--- source-files.js
+++ source-files.js
@@ -1,3 +1,4 @@
-var N = null;var sourcesIndex = {};
-sourcesIndex["casbin"] = {"name":"","dirs":[{"name":"adapter","files":["file_adapter.rs","memory_adapter.rs","mod.rs","null_adapter.rs"]},{"name":"model","files":["assertion.rs","default_model.rs","function_map.rs","mod.rs"]},{"name":"rbac","files":["default_role_manager.rs","mod.rs","role_manager.rs"]}],"files":["config.rs","convert.rs","core_api.rs","effector.rs","emitter.rs","enforcer.rs","error.rs","internal_api.rs","lib.rs","macros.rs","management_api.rs","prelude.rs","rbac_api.rs","util.rs"]};
+var sourcesIndex = JSON.parse('{\
+"casbin":["",[["adapter",[],["file_adapter.rs","memory_adapter.rs","mod.rs","null_adapter.rs"]],["model",[],["assertion.rs","default_model.rs","function_map.rs","mod.rs"]],["rbac",[],["default_role_manager.rs","mod.rs","role_manager.rs"]]],["config.rs","convert.rs","core_api.rs","effector.rs","emitter.rs","enforcer.rs","error.rs","internal_api.rs","lib.rs","macros.rs","management_api.rs","prelude.rs","rbac_api.rs","util.rs"]]\
+}');
 createSourceSidebar();
